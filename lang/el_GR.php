c5e196148989458420c9bf911e2e8399524f9b79
Aaron Carlino <aaron@bluehousegroup.com>
2011-06-22 10:01:07 -0400

    added Greek translation

---
+++ lang/el_GR.php
@@ -0,0 +1,46 @@
+<?php
+
+global $lang;
+
+$lang['el_GR']['ImageGalleryAlbum']['ALBUMNAME'] = 'Oνομασία Album';
+$lang['el_GR']['ImageGalleryAlbum']['ALBUMTITLE'] = 'Tίτλος Album';
+$lang['el_GR']['ImageGalleryAlbum']['COVERIMAGE'] = 'Εικόνα Εξωφύλλου';
+$lang['el_GR']['ImageGalleryAlbum']['DESCRIPTION'] = 'Περιγραφή';
+$lang['el_GR']['ImageGalleryItem']['ALBUM'] = 'Album';
+$lang['el_GR']['ImageGalleryItem']['CAPTION'] = 'Λεζάντα';
+$lang['el_GR']['ImageGalleryManager']['CHOOSEALBUM'] = 'Επιλέξτε ένα album';
+$lang['el_GR']['ImageGalleryManager']['DATAOBJECTMANAGER'] = 'Το ImageGallery απαιτεί το DataObjectManager module.';
+$lang['el_GR']['ImageGalleryManager']['ERROR'] = 'Λάθος';
+$lang['el_GR']['ImageGalleryManager']['ROTATECLOCKWISE'] = 'Περιστροφή clockwise';
+$lang['el_GR']['ImageGalleryManager']['ROTATECOUNTERCLOCKWISE'] = 'Περιστροφή counter-clockwise';
+$lang['el_GR']['ImageGalleryManager']['ROTATING'] = 'Περιστροφή';
+$lang['el_GR']['ImageGalleryManager']['UPLOADFROMPC'] = 'Ανέβασμα εικόνων από τον Η/Υ μου';
+$lang['el_GR']['ImageGalleryManager']['UPLOADIMAGES'] = 'Ανέβασμα εικόνων';
+$lang['el_GR']['ImageGalleryManager']['UPLOADIMAGESTO'] = 'Ανέβασμα εικόνων στο "%s"';
+$lang['el_GR']['ImageGalleryPage']['ALBUM'] = 'Album';
+$lang['el_GR']['ImageGalleryPage']['ALBUMCOVERIMAGES'] = 'Εικόνες εξωφύλλων Album';
+$lang['el_GR']['ImageGalleryPage']['ALBUMS'] = 'Albums';
+$lang['el_GR']['ImageGalleryPage']['ALBUMSNOTSAVED'] = 'Μπορείτε να προσθέσετε albums στο gallery σας από την στιγμή που θα σώσετε την σελίδα για πρώτη φορά.';
+$lang['el_GR']['ImageGalleryPage']['ANALBUM'] = 'ένα Album';
+$lang['el_GR']['ImageGalleryPage']['CONFIGURATION'] = 'Ρυθμίσεις';
+$lang['el_GR']['ImageGalleryPage']['IMAGES'] = 'Εικόνες';
+$lang['el_GR']['ImageGalleryPage']['IMAGESPERPAGE'] = 'Αριθμός εικόνων ανά σελίδα';
+$lang['el_GR']['ImageGalleryPage']['MAXFILES'] = 'Ανώτατος επιτρεπτός αριθμός αρχείων στο upload queue';
+$lang['el_GR']['ImageGalleryPage']['NOALBUMS'] = 'Δεν έχετε albums. Επιλέξτε το Albums tab για να δημιουργήσετε τουλάχιστον ένα album πριν προσθέσετε φωτογραφίες.';
+$lang['el_GR']['ImageGalleryPage']['NORMALSIZE'] = 'Φυσικό μέγεθος (pixels)';
+$lang['el_GR']['ImageGalleryPage']['PHOTOS'] = 'Φωτογραφίες';
+$lang['el_GR']['ImageGalleryPage']['PHOTOSNOTSAVED'] = 'Μπορείτε να προσθέσετε φωτογραφίες στο gallery σας από την στιγμή που θα σώσετε την σελίδα για πρώτη φορά.';
+$lang['el_GR']['ImageGalleryPage']['POPUPSTYLE'] = 'Popup style';
+$lang['el_GR']['ImageGalleryPage']['THUMBNAILSIZE'] = 'Μέγεθος προεπισκόπησης εικόνας (pixels)';
+$lang['el_GR']['ImageGalleryPage']['TO'] = 'στο';
+$lang['el_GR']['ImageGalleryPage.ss']['ALBUMSINGALLERY'] = 'Albums στο %s';
+$lang['el_GR']['ImageGalleryPage_album.ss']['GOTOALBUM'] = 'Πηγαίνετε στο %s album';
+$lang['el_GR']['ImageGalleryPage_album.ss']['NEXT'] = 'Επόμενο';
+$lang['el_GR']['ImageGalleryPage_album.ss']['NEXTALBUM'] = 'Επόμενο Album';
+$lang['el_GR']['ImageGalleryPage_album.ss']['PREVIOUS'] = 'Προηγούμενο';
+$lang['el_GR']['ImageGalleryPage_album.ss']['PREVIOUSALBUM'] = 'Προηγούμενο Album';
+$lang['el_GR']['ImageGalleryPage_album.ss']['VIEWNEXTPAGE'] = 'Δείτε την επόμενη σελίδα';
+$lang['el_GR']['ImageGalleryPage_album.ss']['VIEWPAGENUMBER'] = 'Δείτε την σελίδα με αριθμό %s';
+$lang['el_GR']['ImageGalleryPage_album.ss']['VIEWPREVIOUSPAGE'] = 'Δείτε την προηγούμενη σελίδα';
+
+?>
