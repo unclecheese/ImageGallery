b8fa53a10c04e3cbe9e433d9b5400fa54ff5a091
Francisco arenas <fa@dospuntocero.cl>
2011-09-06 06:02:01 -0300

    typo on english lang, plus spanish translation

---
+++ lang/es_ES.php
@@ -0,0 +1,46 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['ImageGalleryAlbum']['ALBUMNAME'] = 'Nombre del Álbum';
+$lang['en_US']['ImageGalleryAlbum']['ALBUMTITLE'] = 'Título del Álbum';
+$lang['en_US']['ImageGalleryAlbum']['COVERIMAGE'] = 'Portada';
+$lang['en_US']['ImageGalleryAlbum']['DESCRIPTION'] = 'Descripción';
+$lang['en_US']['ImageGalleryItem']['ALBUM'] = 'Álbum';
+$lang['en_US']['ImageGalleryItem']['CAPTION'] = 'Leyenda';
+$lang['en_US']['ImageGalleryManager']['CHOOSEALBUM'] = 'Elegir álbum';
+$lang['en_US']['ImageGalleryManager']['DATAOBJECTMANAGER'] = 'Este módulo necesita el módulo DataObjectManager.';
+$lang['en_US']['ImageGalleryManager']['ERROR'] = 'Error';
+$lang['en_US']['ImageGalleryManager']['ROTATECLOCKWISE'] = 'Rotar según las agujas del reloj';
+$lang['en_US']['ImageGalleryManager']['ROTATECOUNTERCLOCKWISE'] = 'Rotar en contra de las agujas del reloj';
+$lang['en_US']['ImageGalleryManager']['ROTATING'] = 'Rotando';
+$lang['en_US']['ImageGalleryManager']['UPLOADFROMPC'] = 'Subir desde mi computador';
+$lang['en_US']['ImageGalleryManager']['UPLOADIMAGES'] = 'Subir imágenes';
+$lang['en_US']['ImageGalleryManager']['UPLOADIMAGESTO'] = 'Subir imágenes a "%s"';
+$lang['en_US']['ImageGalleryPage']['ALBUM'] = 'Álbum';
+$lang['en_US']['ImageGalleryPage']['ALBUMCOVERIMAGES'] = 'Portadas';
+$lang['en_US']['ImageGalleryPage']['ALBUMS'] = 'Álbums';
+$lang['en_US']['ImageGalleryPage']['ALBUMSNOTSAVED'] = 'Tú puedes agregar álbums a tu galería una vez que la página sea guardada por primera vez.';
+$lang['en_US']['ImageGalleryPage']['ANALBUM'] = 'un Álbum';
+$lang['en_US']['ImageGalleryPage']['CONFIGURATION'] = 'Configuración';
+$lang['en_US']['ImageGalleryPage']['IMAGES'] = 'Imágenes';
+$lang['en_US']['ImageGalleryPage']['IMAGESPERPAGE'] = 'Número de imagenes por página';
+$lang['en_US']['ImageGalleryPage']['MAXFILES'] = 'Máxima cantidad de archivos en la cola de subida';
+$lang['en_US']['ImageGalleryPage']['NOALBUMS'] = 'No tienes álbums. Haz click en la etiqueta Álbums para crear al menos un álbum antes de agregar fotos.';
+$lang['en_US']['ImageGalleryPage']['NORMALSIZE'] = 'Tamaño normal (pixeles)';
+$lang['en_US']['ImageGalleryPage']['PHOTOS'] = 'Fotos';
+$lang['en_US']['ImageGalleryPage']['PHOTOSNOTSAVED'] = 'Tú podrás agregar fotos a tu galería una vez que guardes la página por primera vez.';
+$lang['en_US']['ImageGalleryPage']['POPUPSTYLE'] = 'Estilo de ventana emergente';
+$lang['en_US']['ImageGalleryPage']['THUMBNAILSIZE'] = 'Tamaño de la miniatura (pixeles)';
+$lang['en_US']['ImageGalleryPage']['TO'] = 'a';
+$lang['en_US']['ImageGalleryPage.ss']['ALBUMSINGALLERY'] = 'Álbums en %s';
+$lang['en_US']['ImageGalleryPage_album.ss']['GOTOALBUM'] = 'Ir al álbum %s';
+$lang['en_US']['ImageGalleryPage_album.ss']['NEXT'] = 'Siguiente';
+$lang['en_US']['ImageGalleryPage_album.ss']['NEXTALBUM'] = 'Siguiente álbum';
+$lang['en_US']['ImageGalleryPage_album.ss']['PREVIOUS'] = 'Previo';
+$lang['en_US']['ImageGalleryPage_album.ss']['PREVIOUSALBUM'] = 'Álbum previo';
+$lang['en_US']['ImageGalleryPage_album.ss']['VIEWNEXTPAGE'] = 'Ver la siguiente página';
+$lang['en_US']['ImageGalleryPage_album.ss']['VIEWPAGENUMBER'] = 'Ver página número %s';
+$lang['en_US']['ImageGalleryPage_album.ss']['VIEWPREVIOUSPAGE'] = 'Ver la página anterior';
+
+?>
