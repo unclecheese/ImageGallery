7b5559296feacd5355c7d6d073d24bd7ed2f0612
Juan Molina <juan.molina@club.fr>
2011-11-08 18:09:23 +0100

    Added es_ES and fr_FR translations

---
+++ lang/fr_FR.php
@@ -0,0 +1,95 @@
+<?php
+
+global $lang;
+
+$lang['fr_FR']['AlbumList']['IMAGECOUNT'] = '%s photos';
+$lang['fr_FR']['AlbumList.ss']['PHOTOS'] = 'photos';
+$lang['fr_FR']['ImageGalleryAlbum']['ALBUMNAME'] = 'Nom de l’album';
+$lang['fr_FR']['ImageGalleryAlbum']['ALBUMTITLE'] = 'Titre de l’album';
+$lang['fr_FR']['ImageGalleryAlbum']['COVERIMAGE'] = 'Image de couverture';
+$lang['fr_FR']['ImageGalleryAlbum']['DESCRIPTION'] = 'Description';
+$lang['fr_FR']['ImageGalleryAlbum']['PLURALNAME'] = array(
+	'Albums de la galerie d’images',
+	50,
+	'Plural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['fr_FR']['ImageGalleryAlbum']['SINGULARNAME'] = array(
+	'Album de la galerie d’images',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['fr_FR']['ImageGalleryImage']['PLURALNAME'] = array(
+	'Fichiers',
+	50,
+	'Plural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['fr_FR']['ImageGalleryImage']['SINGULARNAME'] = array(
+	'Fichier',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['fr_FR']['ImageGalleryItem']['ALBUM'] = 'Album';
+$lang['fr_FR']['ImageGalleryItem']['CAPTION'] = 'Légende';
+$lang['fr_FR']['ImageGalleryItem']['PLURALNAME'] = array(
+	'Éléments de la galerie d’images',
+	50,
+	'Plural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['fr_FR']['ImageGalleryItem']['SINGULARNAME'] = array(
+	'Élément de la galerie d’images',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['fr_FR']['ImageGalleryManager']['CHOOSEALBUM'] = 'Choisissez un album';
+$lang['fr_FR']['ImageGalleryManager']['DATAOBJECTMANAGER'] = 'Imagegallery requiert le module DataObjectManager.';
+$lang['fr_FR']['ImageGalleryManager']['ERROR'] = 'Erreur';
+$lang['fr_FR']['ImageGalleryManager']['ROTATECLOCKWISE'] = 'Tournez vers la droite';
+$lang['fr_FR']['ImageGalleryManager']['ROTATECOUNTERCLOCKWISE'] = 'Tournez vers la gauche';
+$lang['fr_FR']['ImageGalleryManager']['ROTATING'] = 'En cours...';
+$lang['fr_FR']['ImageGalleryManager']['UPLOADFROMPC'] = 'Transférer à partir de mon ordinateur';
+$lang['fr_FR']['ImageGalleryManager']['UPLOADIMAGES'] = 'Transférer images';
+$lang['fr_FR']['ImageGalleryManager']['UPLOADIMAGESTO'] = 'Télécharger images dans " %s "';
+$lang['fr_FR']['ImageGalleryPage']['ALBUM'] = 'Album';
+$lang['fr_FR']['ImageGalleryPage']['ALBUMCOVERIMAGES'] = 'Images de couverture de l’album';
+$lang['fr_FR']['ImageGalleryPage']['ALBUMS'] = 'Albums';
+$lang['fr_FR']['ImageGalleryPage']['ALBUMSNOTSAVED'] = 'Vous pourrez ajouter des albums à votre galerie une fois que vous aurez enregistré la page pour la première fois.';
+$lang['fr_FR']['ImageGalleryPage']['ANALBUM'] = 'un album';
+$lang['fr_FR']['ImageGalleryPage']['CONFIGURATION'] = 'Configuration';
+$lang['fr_FR']['ImageGalleryPage']['CROPTOSQUARE'] = 'Rogner les vignettes au carré';
+$lang['fr_FR']['ImageGalleryPage']['HEIGHT'] = 'Hauteur';
+$lang['fr_FR']['ImageGalleryPage']['IMAGES'] = 'Images';
+$lang['fr_FR']['ImageGalleryPage']['IMAGESPERPAGE'] = 'Nombre d’images par page';
+$lang['fr_FR']['ImageGalleryPage']['MAXFILES'] = 'Nombre maximum de fichiers dans la file d’attente de téléchargement';
+$lang['fr_FR']['ImageGalleryPage']['MEDIUMSIZE'] = 'Taille moyenne (pixels)';
+$lang['fr_FR']['ImageGalleryPage']['NOALBUMS'] = 'Vous n’avez aucun album. Faites clic dans l’onglet Albums pour en créer au moins un avant d’ajouter des images.';
+$lang['fr_FR']['ImageGalleryPage']['NORMALHEIGHT'] = 'Hauteur courante (pixels)';
+$lang['fr_FR']['ImageGalleryPage']['NORMALSIZE'] = 'Largeur courante (pixels)';
+$lang['fr_FR']['ImageGalleryPage']['PHOTOS'] = 'Photos';
+$lang['fr_FR']['ImageGalleryPage']['PHOTOSNOTSAVED'] = 'Vous pourrez ajouter des photos à votre galerie une fois que vous aurez enregistré la page pour la première fois.';
+$lang['fr_FR']['ImageGalleryPage']['PLURALNAME'] = array(
+	'Pages de la galerie d’images',
+	50,
+	'Plural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['fr_FR']['ImageGalleryPage']['POPUPSTYLE'] = 'Style de la fenêtre émergente';
+$lang['fr_FR']['ImageGalleryPage']['SINGULARNAME'] = array(
+	'Page de la galerie d’images',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['fr_FR']['ImageGalleryPage']['THUMBNAILHEIGHT'] = 'Hauteur de la vignette (pixels)';
+$lang['fr_FR']['ImageGalleryPage']['TO'] = 'à';
+$lang['fr_FR']['ImageGalleryPage']['VIEWDEMO'] = '(voir démo)';
+$lang['fr_FR']['ImageGalleryPage']['WIDTH'] = 'Largeur';
+$lang['fr_FR']['ImageGalleryPage.ss']['ALBUMSINGALLERY'] = 'Albums dans %s';
+$lang['fr_FR']['ImageGalleryPage_album.ss']['GOTOALBUM'] = 'Aller à l’album %s';
+$lang['fr_FR']['ImageGalleryPage_album.ss']['NEXT'] = 'Suivant';
+$lang['fr_FR']['ImageGalleryPage_album.ss']['NEXTALBUM'] = 'Album suivant';
+$lang['fr_FR']['ImageGalleryPage_album.ss']['PREVIOUS'] = 'Précédent';
+$lang['fr_FR']['ImageGalleryPage_album.ss']['PREVIOUSALBUM'] = 'Album précédent';
+$lang['fr_FR']['ImageGalleryPage_album.ss']['VIEWNEXTPAGE'] = 'Voir la page suivante';
+$lang['fr_FR']['ImageGalleryPage_album.ss']['VIEWPAGENUMBER'] = 'Afficher la page numéro %s';
+$lang['fr_FR']['ImageGalleryPage_album.ss']['VIEWPREVIOUSPAGE'] = 'Voir la page précédente';
+
+?>
+
